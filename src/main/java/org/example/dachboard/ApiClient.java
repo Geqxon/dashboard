c7976f05e265f227465769c3bf0c09c62dd94f70
Geqxon <lars.daams@gmail.com>
2024-06-12 16:37:46 +0200

    grafiek werkend

--- src/main/java/org/example/dachboard/ApiClient.java
+++ src/main/java/org/example/dachboard/ApiClient.java
@@ -12,8 +12,8 @@ import java.util.List;
 
 public class ApiClient {
 
-    private static final String BASE_URL = "http://ladaams.gcmsi.nl/api"; // verander dit naar de url van jouw api op gcmsi bijvoorbeeldl http://kreitsema.gcmsi.nl/api/
-    private static final String API_KEY = "Welkom01"; // Verander dit naar je echte API key
+    private static final String BASE_URL = "http://ladaams.gcmsi.nl/api/"; // verander dit naar de url van jouw api op gcmsi bijvoorbeeldl http://kreitsema.gcmsi.nl/api/
+    private static final String API_KEY = "jaaaalaaaa"; // Verander dit naar je echte API key
     private final HttpClient client;
 
     public ApiClient() {
@@ -23,6 +23,7 @@ public class ApiClient {
     // Haal de laatste waarde op van de gegeven sensor
     public Float getLastValueFromSensor(int sensorID) throws Exception {
         String url = BASE_URL + "metingen/filters?aantal=1&sorteerVolgorde=DESC&sensorID=" + sensorID;
+        System.out.println(url);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(url))
                 .header("Authorization", API_KEY)
@@ -41,10 +42,9 @@ public class ApiClient {
         }
         return null;
     }
-
-    // Haal de laatste 10 waarden op van een specifieke sensor, alleen met waarde en timestamp
-    public List<JSONObject> getLastTenValuesFromSensor(int sensorID) throws Exception {
-        String url = BASE_URL + "metingen/filters?aantal=10&sorteerVolgorde=DESC&sensorID=" + sensorID;
+    // grafiek sensoren 24 uur optehalen.
+    public List<JSONObject> getLast24hFromSensor(int sensorID) throws Exception {
+        String url = BASE_URL + "metingen/filters?aantal=48&sorteerVolgorde=DESC&sensorID=" + sensorID;
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(url))
                 .header("Authorization", API_KEY)
@@ -68,10 +68,37 @@ public class ApiClient {
         }
     }
 
+    // Haal de laatste 10 waarden op van een specifieke sensor, alleen met waarde en timestamp
+    public List<JSONObject> getLastTenValuesFromSensor(int sensorID) throws Exception {
+        String url = BASE_URL + "metingen/filters?aantal=10&sorteerVolgorde=DESC&sensorID=" + sensorID;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .header("Authorization", API_KEY)
+                .build();
+
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            JSONArray metingen = new JSONArray(response.body());
+            List<JSONObject> result = new ArrayList<>();
+            for (int i = 0; i < metingen.length(); i++) {
+                JSONObject meting = metingen.getJSONObject(i);
+                JSONObject filteredMeting = new JSONObject();
+                filteredMeting.put("Waarde", meting.getFloat("Waarde"));
+                filteredMeting.put("Timestamp", meting.getString("Timestamp"));
+                result.add(filteredMeting);
+            }
+            return result;
+        } else {
+            throw new RuntimeException("Error: " + response.statusCode() + " - " + response.body());
+        }
+    }
+
     // Haal alle sensoren op
     public JSONArray getAllSensors() throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI(BASE_URL + "sensoren"))
+                .uri(new URI(BASE_URL + "Sensoren"))
                 .header("Authorization", API_KEY)
                 .build();
 
@@ -92,7 +119,7 @@ public class ApiClient {
         newSensor.put("Diepte", diepte);
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI(BASE_URL + "sensoren"))
+                .uri(new URI(BASE_URL + "Sensoren"))
                 .header("Authorization", API_KEY)
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(newSensor.toString()))
--- src/main/java/org/example/dachboard/HelloController.java
+++ src/main/java/org/example/dachboard/HelloController.java
@@ -1,14 +1,80 @@
 package org.example.dachboard;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import java.util.List;
+import org.json.JSONObject;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private LineChart chartSensor;
+
+    private final ApiClient client = new ApiClient();
+
+    @FXML
+    public void initialize() throws Exception {
+        System.out.println("initialize");
+        List<JSONObject> lastTen = client.getLastTenValuesFromSensor(1);
+        System.out.println( lastTen);
+    }
+
+    public void btnSensor1(ActionEvent actionEvent) throws Exception {
+        ApiClient apiClient = new ApiClient();
+        chartSensor.getData().clear();
+
+        int sensorID = 1;
+        List<JSONObject> data = apiClient.getLast24hFromSensor(sensorID);
+        XYChart.Series<String, Number> series = null;
+        series = new XYChart.Series<>();
+        chartSensor.getData().add(series);
+
+        for (JSONObject jsonObject : data) {
+            String timestamp = jsonObject.getString("Timestamp");
+            Float Waarde = jsonObject.getFloat("Waarde");
+            series.getData().add(new XYChart.Data<>(timestamp, Waarde));
+            System.out.println("timestamp: " + timestamp + " Waarde: " + Waarde);
+        }
+    }
+
+    public void btnSensor2(ActionEvent actionEvent) throws Exception {
+        ApiClient apiClient = new ApiClient();
+        chartSensor.getData().clear();
+
+        int sensorID = 2;
+        List<JSONObject> data = apiClient.getLast24hFromSensor(sensorID);
+        XYChart.Series<String, Number> series = null;
+        series = new XYChart.Series<>();
+        chartSensor.getData().add(series);
+
+        for (JSONObject jsonObject : data) {
+            String timestamp = jsonObject.getString("Timestamp");
+            Float Waarde = jsonObject.getFloat("Waarde");
+            series.getData().add(new XYChart.Data<>(timestamp, Waarde));
+        }
+    }
+
+
+    public void btnSensor3(ActionEvent actionEvent) throws Exception {
+        ApiClient apiClient = new ApiClient();
+        chartSensor.getData().clear();
+
+        int sensorID = 3;
+        List<JSONObject> data = apiClient.getLast24hFromSensor(sensorID);
+        XYChart.Series<String, Number> series = null;
+        series = new XYChart.Series<>();
+        chartSensor.getData().add(series);
+
+        for (JSONObject jsonObject : data) {
+            String timestamp = jsonObject.getString("Timestamp");
+            Float Waarde = jsonObject.getFloat("Waarde");
+            series.getData().add(new XYChart.Data<>(timestamp, Waarde));
+        }
+    }
+
+    public void btnReloadPage(ActionEvent actionEvent) {
+
     }
 }
